23f58b09bcba6ed04880c03751d8fc19691d24d0
Adrian Hansa <adrian.hansa@yahoo.com>
2022-03-13 16:56:41 +0000

    Created Product model

---
+++ models/Product.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    image: { type: String, required: true },
+    brand: { type: String, required: true },
+    category: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    rating: { type: Number, required: true, default: 0 },
+    numReviews: { type: Number, required: true, default: 0 },
+    countInStock: { type: Number, required: true, default: 0 },
+  },
+  { timestamps: true }
+);
+
+const Product =
+  mongoose.models('products') || mongoose.model('products', productSchema);
+
+export default Product;
